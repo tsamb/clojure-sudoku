085f0367ec85a041c9342cb1f90c607ac5ad4a9c
Sam Blackman <sblackman@gmail.com>
2019-07-11 19:32:20 -0400

    Playing around with solving and recursion

--- sudoku.clj
+++ sudoku.clj
@@ -15,13 +15,16 @@
   (vec (range (row-start i) (+ 9 (row-start i)))))
 
 (defn row-values [board i]
-  (mapv board (row-slice i)))
+  (map board (row-slice i)))
 
 (defn col-start [i]
   (mod i 9))
 
 (defn col-slice [i]
   (mapv (fn [num] (+ num (col-start i))) [0 9 18 27 36 45 54 63 72]))
+
+(defn col-values [board i]
+  (map board (col-slice i)))
 
 (defn box-col [i]
   (mod (quot i 3) 3))
@@ -35,6 +38,9 @@
 (defn box-slice [i]
   (mapv (fn [num] (+ (box-start i) num)) [0 1 2 9 10 11 18 19 20]))
 
+(defn box-values [board i]
+  (map board (box-slice i)))
+
 (defn str-to-board [input-board]
     (mapv to-possibles
       (take 81
@@ -42,13 +48,42 @@
 
 (defn input [] "...28.94.1.4...7......156.....8..57.4.......8.68..9.....196......5...8.3.43.28...")
 
-(map-indexed (fn [i possible]
-  i
-  )
-    (str-to-board (input)))
+(defn pare-possibles [possibles confirmed-set]
+  (set/difference possibles confirmed-set))
 
-(str-to-board (input))
+(defn confirmed-for [board i]
+  (set/union
+    (reduce set/union (filter confirmed? (row-values board i)))
+    (reduce set/union (filter confirmed? (col-values board i)))
+    (reduce set/union (filter confirmed? (box-values board i)))))
 
+; (defn row-col-box-values [board i]
+;   )
+
+; (defn possibles-for [board i]
+;   (reduce set/union (filter confirmed? (row-values board i))))
+
+; (set/difference ((load-file "sudoku.clj") 0) (reduce set/union (filter confirmed? (row-values (load-file "sudoku.clj") 0))))
+
+(defn deduce [board]
+  (vec
+    (map-indexed (fn [i possibles]
+      (if (confirmed? possibles)
+        possibles
+        (pare-possibles possibles (confirmed-for board i)))
+      ) board)))
+
+(defn solved? [board]
+  (every? confirmed? board))
+
+(defn solve [board]
+  (if (solved? board)
+    board
+    (solve (deduce board))))
+
+; (str-to-board (input))
+
+(defn b [] (str-to-board (input)))
 
 
 ; DONE:
